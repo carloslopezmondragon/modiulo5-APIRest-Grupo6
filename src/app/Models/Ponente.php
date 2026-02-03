a6a025d311934d9925cb9e15eb27ece9c1be4045
carloslopezmondragon <carlos.mondragon82@gmail.com>
2026-02-03 19:41:40 +0000

    Agregadas entidades Ponente y Asistente a la API

---
+++ src/app/Models/Ponente.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class Ponente extends Model
+{
+
+    protected $table = 'ponentes';
+    protected $fillable = [
+        'nombre',
+        'biografi a',
+        'especialidad',
+    ];
+}
